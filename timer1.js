aa51c4f48998eb558a9aba9458ac3b47508b9d93
isissi <isissi_525@hotmail.com>
2021-11-10 18:13:07 -0700

    update

--- timer1.js
+++ timer1.js
@@ -3,9 +3,22 @@ let times = input.slice(2);
 
 times.sort((a, b) => a - b);
 
+let validTimes = [];
+
 for (let time of times) {
-  time *= 1000;
-  setTimeout(() => {
-    process.stdout.write("\x07");
-  }, time);
+  if (time > 0 || Number.isInteger(time)) {
+    validTimes.push(time);
+  }
+
+  if (validTimes.length === 0) {
+    console.log("No invalid time provided");
+    return false;
+  } else {
+    for (let time of validTimes) {
+      time *= 1000;
+      setTimeout(() => {
+        process.stdout.write("\x07");
+      }, time);
+    }
+  }
 }
